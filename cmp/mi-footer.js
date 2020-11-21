c513cf3708bb58c177faaa7471f247a9e84e44cf
stephanieMendoza <stephaniemendoza220797@gmail.com>
2020-11-20 19:14:20 -0600

    cambios E

--- cmp/mi-footer.js
+++ cmp/mi-footer.js
@@ -1,1 +1,7 @@
-class Mifooter 
+class MiFooter extends HTMLElement {
+    connectedCallback() {
+      this.innerHTML = /* html */
+        `Copyright &copy; 2020 Arroyo Mendoza Stephanie.`;
+    }
+  }
+  customElements.define("mi-footer", MiFooter);
